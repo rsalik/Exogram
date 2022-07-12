715c234dbea831ceeb0735e92b8396d7019bf53e
Ryan Salik <rssalik14@gmail.com>
2022-07-12 14:47:11 -0400

    Dictionary Definitions on Hover fixes

--- client/src/components/DefinableTerm.tsx
+++ client/src/components/DefinableTerm.tsx
@@ -4,23 +4,25 @@ import dictionaryDefinitions from '../dictionaryDefinitions.json';
 export function generateDefinableTermsFromText(text: string) {
   return (
     <>
-      {text.split(' ').map<React.ReactNode>((t) => {
-        let tr = t.trim().replaceAll(',', '');
+      {text
+        .replaceAll(/,(?=[^\s])/g, ', ') // Add spaces to commas w/o spaces so that terms are seperated
+        .split(' ')
+        .map<React.ReactNode>((t) => {
+          let tr = t.trim().replaceAll(',', '');
 
-        if (tr.length === 0) {
-          return <></>;
-        }
+          if (tr.length === 0) {
+            return <></>;
+          }
 
-        let dictTerm = dictionaryDefinitions.find(
-          (d) => d.name === tr || d.name.replaceAll('(p)', 'p') === tr
-        );
+          let dictTerm = dictionaryDefinitions.find((d) => d.name === tr || d.name.replaceAll('(p)', 'p') === tr);
 
-        if (dictTerm) {
-          return <DefinableTerm text={t} term={dictTerm.name} definition={dictTerm.def} />;
-        }
+          if (dictTerm) {
+            return <DefinableTerm text={t} term={dictTerm.name} definition={dictTerm.def} />;
+          }
 
-        return <>{t}</>;
-      }).reduce((p, c) => [p, ' ', c])}
+          return <>{t}</>;
+        })
+        .reduce((p, c) => [p, ' ', c])}
     </>
   );
 }
