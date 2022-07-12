0df790763242ebae8ea529f7cd419c1fd1dd7146
Ryan Salik <rssalik14@gmail.com>
2022-07-12 15:44:37 -0400

    Glass effect fixes

--- client/src/components/TicDisposition.tsx
+++ client/src/components/TicDisposition.tsx
@@ -1,10 +1,12 @@
+import { generateDefinableTermsFromText } from "./DefinableTerm";
+
 export default function TicDisposition(props: { data: any }) {
   return (
     <div className="disposition-wrapper">
       <div className="disposition">
         {props.data.name && <div className="name">{props.data.name}</div>}
         <div className="value">{props.data.disposition}</div>
-        {props.data.comments && <div className="comments">{props.data.comments}</div>}
+        {props.data.comments && <div className="comments">{generateDefinableTermsFromText(props.data.comments)}</div>}
       </div>
     </div>
   );
