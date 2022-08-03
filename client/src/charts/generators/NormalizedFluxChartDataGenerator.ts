0de5e2155317e4c1c5567dec821fa69971657d79
Ryan Salik <rssalik14@gmail.com>
2022-08-03 14:30:13 -0400

    Fix Clean Normalized Flux series

--- client/src/charts/generators/NormalizedFluxChartDataGenerator.ts
+++ client/src/charts/generators/NormalizedFluxChartDataGenerator.ts
@@ -10,26 +10,24 @@ export class NormalizedFluxChartDataGenerator extends ChartDataGenerator {
   generateDataset(data: any, ticId: string, index: number, cleaned?: boolean) {
     if (!data || data.error) return null;
 
-    return [{
-      label: cleaned ? `TIC ${ticId} (clean)` : 'TIC ' + ticId,
-      data: data.time.map((v: any, i: number) => {
-        return cleaned
-          ? {
-              x: v,
-              y: data.flux_clean[i] || null,
-            }
-          : {
-              x: v,
-              y: data.flux[i] || null,
-            };
-      }),
-      showLine: true,
-      borderColor: this.colorArr[(index * 2) % this.colorArr.length],
-      backgroundColor: this.colorArr[(index * 2) % this.colorArr.length],
-      tension: 0.4,
-      borderWidth: 2,
-      pointRadius: 1,
-    }];
+    return [
+      {
+        label: cleaned ? `TIC ${ticId} (clean)` : 'TIC ' + ticId,
+        data: cleaned
+          ? data.time_clean.map((v: any, i: number) => {
+              return { x: v, y: data.flux_clean[i] || null };
+            })
+          : data.time.map((v: any, i: number) => {
+              return { x: v, y: data.flux[i] || null };
+            }),
+        showLine: true,
+        borderColor: this.colorArr[(index * 2) % this.colorArr.length],
+        backgroundColor: this.colorArr[(index * 2) % this.colorArr.length],
+        tension: 0.4,
+        borderWidth: 2,
+        pointRadius: 1,
+      },
+    ];
   }
 
   getDefaultRange() {
