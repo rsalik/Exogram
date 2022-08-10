a33b81cb9ae662f27c832e729a0ca047fd98fe47
Ryan Salik <rssalik14@gmail.com>
2022-08-10 13:14:32 -0400

    Update file searching for single folder file structure

--- api/index.ts
+++ api/index.ts
@@ -22,76 +22,33 @@ app.get('/api/getTicFiles/:ticId', async (req, res) => {
 
   const drive = google.drive({ version: 'v3', auth });
 
-  let files = [] as any[];
-  const folderList = (await recursiveGetSubfolders('1Z74BU-ijJy710QA3M9YwE_l1cE_dpSHA', drive)) as any[];
+  const files = await getTicFiles(req.params.ticId, drive);
 
-  for await (const folder of folderList) {
-    const newFiles = await getTicFilesFromFolder(req.params.ticId.toString(), drive, folder.id);
-    if (newFiles) files = files.concat(newFiles);
+  if (!files || !files.length) {
+    res.send([]);
+    return;
   }
 
   res.send(
     files.map((f) => ({
       ...f,
-      webContentLink: f.webContentLink.replace(/&export=download/g, ''),
+      webContentLink: f.webContentLink?.replace(/&export=download/g, ''),
     }))
   );
 });
 
-async function recursiveGetSubfolders(folderId: string, drive: drive_v3.Drive, pageToken?: string) {
-  return new Promise((resolve, reject) => {
-    let folderIds: any[] = [];
-
-    drive.files.list(
-      {
-        q: `'${folderId}' in parents and mimeType = 'application/vnd.google-apps.folder'`,
-        pageSize: 1000,
-        fields: 'nextPageToken, files(id, webContentLink, name, mimeType)',
-        pageToken: pageToken,
-      },
-      async (err: any, driveRes: any) => {
-        if (err) {
-          console.error('The API returned an error: ' + err);
-          reject(err);
-        }
-
-        if (driveRes.data.nextPageToken) {
-          folderIds = folderIds.concat(await recursiveGetSubfolders(folderId, driveRes.data.nextPageToken));
-        }
-
-        for await (const file of driveRes.data.files) {
-          // Skip TIC-specific folders
-          /*if (file.name.match(/^\d+$/)) {
-            continue;
-          }*/
-
-          folderIds = folderIds.concat(await recursiveGetSubfolders(file.id, drive));
-        }
-
-        folderIds = folderIds.concat(driveRes.data.files);
-        resolve(folderIds);
-      }
-    );
-  });
-}
-
-function getTicFilesFromFolder(ticId: string, drive: drive_v3.Drive, folderId = '1Z74BU-ijJy710QA3M9YwE_l1cE_dpSHA') {
+function getTicFiles(ticId: string, drive: drive_v3.Drive): Promise<drive_v3.Schema$File[] | null> {
   return new Promise((resolve, reject) => {
     drive.files.list(
       {
-        q: `'${folderId}' in parents and name contains '${ticId}' and mimeType = 'application/pdf'`,
+        q: `'1h_9ylFGZwaWkovcxBYTduTtgo6DPuqtN' in parents and name contains '${ticId}' and mimeType = 'application/pdf'`,
         pageSize: 1000,
-        fields: 'nextPageToken, files(id, webContentLink, name, mimeType)',
+        fields: 'files(id, webContentLink, name, mimeType)',
       },
       async (err: any, driveRes: any) => {
         if (err) reject(console.error(err));
 
         let files = driveRes.data.files;
-
-        if (driveRes.data.nextPageToken) {
-          files = files.concat(await getTicFilesFromFolder(ticId, drive, folderId));
-        }
-
         resolve(files);
       }
     );
